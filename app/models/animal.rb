7673907e3ba0f70a3b5db9019aab618eb56476f3
Jessica Roman <jessicaroman.mailpro@gmail.com>
2023-06-05 17:49:44 +0200

    model validations
    Signed-off-by: Jessica Roman <jessicaroman.mailpro@gmail.com>

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,4 +1,8 @@
 class Animal < ApplicationRecord
   belongs_to :taxonomy
   has_many :catches
+  validates :name, presence: true
+  validates :scientific_name, presence: true
+  validates :description, presence: true
+  validates :location, presence: true
 end
--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,2 +1,6 @@
 class Badge < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :condition, presence: true
+  validates :category, presence: true
 end
