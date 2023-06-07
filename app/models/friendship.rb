14725b67cfe157e6adc20f3e2cc28e14124d8380
Jessica Roman <jessicaroman.mailpro@gmail.com>
2023-06-07 08:20:59 +0200

    error with a comma fixed in model
    Signed-off-by: Jessica Roman <jessicaroman.mailpro@gmail.com>

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,6 +1,6 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: "User",
-                      foreign_key: "friend_id",
+                      foreign_key: "friend_id"
   validates :status, presence: true
 end
