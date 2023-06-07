a4c7b63809984904924777102bfdd42258883b47
Mathieu GUILLOT <mathieu.guillot@gaminglink.fr>
2023-06-07 15:51:44 +0200

    application controller : add rescue to return an error message with policy name, method and details / application policy : add raise error for unauthorized user in initializer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,16 @@ class ApplicationController < ActionController::Base
   after_action :verify_authorized, except: [:index], unless: :skip_pundit?
   after_action :verify_policy_scoped, only: [:index], unless: :skip_pundit?
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   private
+
+  def user_not_authorized
+    policy_name = exception.policy.class.to_s.underscore
+
+    flash[:error] = t "#{policy_name}.#{eexception.query}", scope: "pundit", default: :default
+    redirect_back(fallback_location: root_path)
+  end
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -4,6 +4,8 @@ class ApplicationPolicy
   attr_reader :user, :record
 
   def initialize(user, record)
+    raise Pundit::NotAuthorizedError, "must be logged in" unless user
+
     @user = user
     @record = record
   end
@@ -38,6 +40,8 @@ class ApplicationPolicy
 
   class Scope
     def initialize(user, scope)
+      raise Pundit::NotAuthorizedError, "must be logged in" unless user
+
       @user = user
       @scope = scope
     end
