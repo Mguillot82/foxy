c42008a74e83e7c80fb10092b163d6e5cf06c2fe
Julien Miguel <miguel.julien.pro@gmail.com>
2023-06-07 12:15:49 +0200

    add controller taxonomy + policy
    Signed-off-by: Julien Miguel <miguel.julien.pro@gmail.com>

--- app/controllers/taxonomies_controller.rb
+++ app/controllers/taxonomies_controller.rb
@@ -1,2 +1,13 @@
 class TaxonomiesController < ApplicationController
+  def create
+    # name = params[:taxonomy][:name]
+    @taxonomy = Taxonomy.new(taxonomy_params)
+    authorize @taxonomy if @taxonomy.save
+  end
+
+  private
+
+  def taxonomy_params
+    params.require(:taxonomy).permit(:name)
+  end
 end
