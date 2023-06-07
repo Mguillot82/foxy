c4914b38097cb94604f9fb70d03cc37bbd2529c6
Jessica Roman <jessicaroman.mailpro@gmail.com>
2023-06-07 16:11:01 +0200

    controller ongoing
    Signed-off-by: Jessica Roman <jessicaroman.mailpro@gmail.com>

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,7 +1,7 @@
 class Collection < ApplicationRecord
   belongs_to :user
   has_many :collections_catches, dependent: :destroy
-  has_many :catches, through: :collection_catches
+  has_many :catches, through: :collections_catches
 
   validates :name, presence: true
   validates :description, presence: true
