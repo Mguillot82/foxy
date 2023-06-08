bf50c50c8ad74b12e37ef32c15247480127ad1f1
Mathieu GUILLOT <mathieu.guillot@gaminglink.fr>
2023-06-08 15:13:14 +0200

    geocoder in GEMFILE, bundle install, model catch updated with geocode_by

--- Gemfile
+++ Gemfile
@@ -35,6 +35,9 @@ gem "jbuilder"
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
+# Complete geocoding solution for Ruby  [http://www.rubygeocoder.com/]
+gem "geocoder"
+
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
 
--- app/models/catch.rb
+++ app/models/catch.rb
@@ -1,9 +1,12 @@
 class Catch < ApplicationRecord
   belongs_to :animal
   belongs_to :user
-
   has_many :collections_catches, dependent: :destroy
   has_one_attached :photo
 
   validates :location, presence: true
+
+  geocoded_by :city_location
+  after_validation :geocode, if:
+  :will_save_change_to_address?
 end
