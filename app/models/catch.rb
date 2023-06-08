6f6d984157c193d34fc3eee6781faa08395b2390
Mguillot82 <46008427+Mguillot82@users.noreply.github.com>
2023-06-08 16:04:22 +0200

    Update catch.rb

--- app/models/catch.rb
+++ app/models/catch.rb
@@ -6,7 +6,6 @@ class Catch < ApplicationRecord
 
   validates :location, presence: true
 
-  geocoded_by :location
-  after_validation :geocode, if:
-  :will_save_change_to_address?
+  reverse_geocoded_by :latitude, :longitude
+  after_validation :reverse_geocode
 end
