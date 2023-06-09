b9838a63fd8594d7ddf774a9b2e0f62cabd910fd
Bastien Pailloux <bastien.pailloux@hotmail.fr>
2023-06-09 12:56:46 +0200

    Full js done

---
+++ app/policies/catch_policy.rb
@@ -0,0 +1,12 @@
+class CatchPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+  end
+
+  def create?
+    record.user.id == user.id
+  end
+end
