80fceb5b682c6b17b6a2a01bcb5799136daaab08
Jessica Roman <jessicaroman.mailpro@gmail.com>
2023-06-10 19:07:36 +0200

    collection general & add animal
    Signed-off-by: Jessica Roman <jessicaroman.mailpro@gmail.com>

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,5 +2,5 @@ class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: "User",
                       foreign_key: "friend_id"
-  validates :status, inclusion: { in: ["pending", "accepted", "rejected"] }
+  validates :status, inclusion: { in: ["pending", "accepted", "refused"] }
 end
