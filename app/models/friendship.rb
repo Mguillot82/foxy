9665605b99a9d9c45e2ddc42aac5053ccaa1a25c
BastienPailloux <35964611+BastienPailloux@users.noreply.github.com>
2023-06-12 09:04:15 +0200

    Update friendship.rb

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,5 +2,5 @@ class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: "User",
                       foreign_key: "friend_id"
-  validates :status, inclusion: { in: ["pending", "accepted", "refused"] }
+  validates :status, inclusion: { in: ["pending", "accepted", "rejected"] }
 end
