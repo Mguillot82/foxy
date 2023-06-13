be87750cff4c92362250f00b5d334eb786d00a2a
Bastien Pailloux <bastien.pailloux@hotmail.fr>
2023-06-13 17:26:02 +0200

    change in stimulus

--- app/javascript/controllers/friend_card_controller.js
+++ app/javascript/controllers/friend_card_controller.js
@@ -1,5 +1,5 @@
 // app/javascript/controllers/friend_card_controller.js
-import { Controller } from "stimulus";
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   removeCard() {
